4e94385d9b611e593cff510efd836f584fb04abf
Felix.A <kiwibirdi@hotmail.com>
2020-04-06 13:09:48 +0200

    splitscreen iteration v2

--- NinjaFoxGame/Intermediate/Build/Win64/UE4Editor/Inc/NinjaFoxGame/NinjaFoxGame.init.gen.cpp
+++ NinjaFoxGame/Intermediate/Build/Win64/UE4Editor/Inc/NinjaFoxGame/NinjaFoxGame.init.gen.cpp
@@ -21,8 +21,8 @@ void EmptyLinkFunctionForGeneratedCodeNinjaFoxGame_init() {}
 				nullptr,
 				0,
 				PKG_CompiledIn | 0x00000000,
-				0xB885AC2F,
-				0x1340BFEA,
+				0xDCA02C92,
+				0xC7395037,
 				METADATA_PARAMS(nullptr, 0)
 			};
 			UE4CodeGen_Private::ConstructUPackage(ReturnPackage, PackageParams);
